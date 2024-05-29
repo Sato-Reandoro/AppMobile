d3a9c3e60bbe586aac89a6011506d23a285949a6
Leandro Júnior <119981487+Sato-Reandoro@users.noreply.github.com>
2024-05-29 15:16:31 -0300

    deu um erro lá nada haver

--- my_api/app/database.py
+++ my_api/app/database.py
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream:my_api/app/core/database.py
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-from core.configs import settings
-
-# Atualizando a URL do banco de dados para a fornecida
-settings.DB_URL = "postgresql+asyncpg://postgres:2046@localhost:5432/AppMobile"
-
-# Criando o motor de banco de dados assíncrono
-engine: AsyncEngine = create_async_engine(settings.DB_URL)
-
-# Criando a fábrica de sessões assíncronas
-Session: AsyncSession = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False,
-    class_=AsyncSession,
-    bind=engine
-)
-=======
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -35,4 +14,3 @@ def get_db():
         yield db
     finally:
         db.close()
->>>>>>> Stashed changes:my_api/app/database.py
